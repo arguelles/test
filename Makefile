18b640c1cc5419ae8cd9a332955e9c369a8a87d2
Carlos Arguelles <caad@mit.edu>
2016-11-23 15:12:55 -0600

    updated main file

--- Makefile
+++ Makefile
@@ -7,10 +7,12 @@ CFLAGS = -Wall -g -std=c++11
 # define any directories containing header files
 
 INCLUDES = -I/Users/marjon/Dropbox\ \(MIT\)/work/IceCube/decay/neutrino_decay/verosimilitud/inc -I/usr/include/python2.7 -I/usr/local/Cellar/hdf5/1.8.16_1/include -I/Users/marjon/local/dlib-18.18
+INCLUDES += -I/Users/carguelles/DropboxMIT/NeutrinoDecay/verosimilitud/inc
 
 # define any library paths in addition to /usr/lib
 LFLAGS = -L/usr/local/Cellar/hdf5/1.8.16_1/lib
 LFLAGS += -L/Users/marjon/Dropbox\ \(MIT\)/work/IceCube/decay/neutrino_decay/verosimilitud/
+LFLAGS += -L/Users/carguelles/DropboxMIT/NeutrinoDecay/verosimilitud/
 
 # define any libraries to link into executable:
 LIBS = -lpython2.7 -lm -lhdf5 -lhdf5_cpp 
--- test.cpp
+++ test.cpp
@@ -18,273 +18,246 @@
 #include <dlib/optimization.h>
 #include <dlib/member_function_pointer.h>
 
-int main()
-{
+int main() {
 
-std::shared_ptr<Verosimilitud> Vp = std::make_shared<Verosimilitud>(3,0,2);
+  // conventional_flux.h5 contains the DOM effiency correction of the Weaver Sample. Thanks Chris.
+  std::shared_ptr<Verosimilitud> Vp = std::make_shared<Verosimilitud>(3, 0, 2,"./data/Marjon_Int_HondaGaisser.h5","./data/effective_area.h5","./data/conventional_flux.h5");
 
-std::vector<double> perm_param = {2.0, 0.01, 1.0, 1.0};
-std::vector<double> param {2.0, 0.01, 1.0, 1.0};
+  std::vector<double> perm_param = {2.0, 0.01, 1.0, 1.0};
+  std::vector<double> param{2.0, 0.01, 1.0, 1.0};
 
-std::vector<double> loop_low ={1.0, -.1, 0.5, 0.825};
-std::vector<double> loop_high={3.0, .05, 2.0, 1.125};
+  std::vector<double> loop_low = {1.0, -.1, 0.5, 0.825};
+  std::vector<double> loop_high = {3.0, .05, 2.0, 1.125};
 
-std::vector<double> low_bound = {0.01, -0.15, 0.5, 0.875};
-std::vector<double> high_bound = {10.0, 10.00, 10.0, 10.0};
+  std::vector<double> low_bound = {0.01, -0.15, 0.5, 0.875};
+  std::vector<double> high_bound = {10.0, 10.00, 10.0, 10.0};
 
-std::vector<bool> perm_param_to_minimize = {true, true, true, true};
-std::vector<bool> param_to_minimize = {true, true, true, true};
+  std::vector<bool> perm_param_to_minimize = {true, true, true, true};
+  std::vector<bool> param_to_minimize = {true, true, true, true};
 
+  for (unsigned int i = 0; i < 4; i++) {
 
-for (unsigned int i = 0; i<4; i++){
+    for (double prof_param = loop_low[i]; prof_param < loop_high[i];
+         prof_param += (loop_high[i] - loop_low[i]) / 5000.0) {
 
-	for (double prof_param = loop_low[i]; prof_param < loop_high[i]; prof_param += (loop_high[i]-loop_low[i])/5000.0){
-	
-	
-	
-		param = perm_param;
-		param[i] = prof_param;
-	
-		param_to_minimize = perm_param_to_minimize;
-		param_to_minimize[i] = false;
+      param = perm_param;
+      param[i] = prof_param;
 
-		//std::cout << "================ BEGIN CODE =================" <<std::endl;
+      param_to_minimize = perm_param_to_minimize;
+      param_to_minimize[i] = false;
 
-		dlib::matrix<double,0,1> r(4);
-		for(size_t i=0; i<param.size(); i++) r(i)=param[i];
-		// std::cout << "Chi2Initial: "<< std::endl << Vp->Chi2(r) <<std::endl;
-		// std::cout << "Chi2GradientInitial: "<< std::endl << Vp->Chi2Gradient(r) <<std::endl;
-		// 
-		// std::cout << "================ BEGIN MINIMIZATION =================" <<std::endl;
+      // std::cout << "================ BEGIN CODE ================="
+      // <<std::endl;
 
-		std::vector<double> result = Vp->MinLLH(param, low_bound, high_bound, param_to_minimize);
+      dlib::matrix<double, 0, 1> r(4);
+      for (size_t i = 0; i < param.size(); i++)
+        r(i) = param[i];
+      // std::cout << "Chi2Initial: "<< std::endl << Vp->Chi2(r) <<std::endl;
+      // std::cout << "Chi2GradientInitial: "<< std::endl << Vp->Chi2Gradient(r)
+      // <<std::endl;
+      //
+      // std::cout << "================ BEGIN MINIMIZATION ================="
+      // <<std::endl;
 
-		// std::cout << "================ END MINIMIZATION =================" <<std::endl;
+      std::vector<double> result =
+          Vp->MinLLH(param, low_bound, high_bound, param_to_minimize);
 
-		// for(unsigned int i=0; i<result.size(); i++){
-		// std::cout << result[i] << " ";
-		// }
+      // std::cout << "================ END MINIMIZATION ================="
+      // <<std::endl;
 
-//		std::cout<< prof_param << "," << result[result.size()-1] << "," ;
+      // for(unsigned int i=0; i<result.size(); i++){
+      // std::cout << result[i] << " ";
+      // }
 
+      //		std::cout<< prof_param << "," << result[result.size()-1]
+      //<< "," ;
+    }
 
-	}
-	
-	std::cout<<std::endl;
-	std::cout<<std::endl;
-	std::cout<<std::endl;
-	
-}
-std::cout<<std::endl;
+    std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << std::endl;
+  }
+  std::cout << std::endl;
 
+  /*
+  std::shared_ptr<Verosimilitud> Vp = std::make_shared<Verosimilitud>(3,0,2);
 
+  //std::vector<double> Verosimilitud::MinLLH(std::vector<double> param,
+  std::vector<double> low_bound, std::vector<double> high_bound,
+  std::vector<bool> param_to_minimize)
 
+  std::vector<double> param = {1.0, 0.01, 1.0, 1.0};
+  std::vector<double> low_bound = {0.01, -0.05, 0.5, 0.875};
+  std::vector<double> high_bound = {3.0, 0.05, 1.5, 1.125};
+  std::vector<bool> param_to_minimize = {true, true, true, true};
 
+  std::cout << "================ BEGIN CODE =================" <<std::endl;
 
+  dlib::matrix<double,0,1> r(4);
+  for(size_t i=0; i<param.size(); i++) r(i)=param[i];
+  std::cout << "Chi2Initial: "<< std::endl << Vp->Chi2(r) <<std::endl;
+  std::cout << "Chi2GradientInitial: "<< std::endl << Vp->Chi2Gradient(r)
+  <<std::endl;
 
+  std::cout << "================ BEGIN MINIMIZATION ================="
+  <<std::endl;
 
+  std::vector<double> result = Vp->MinLLH(param, low_bound, high_bound,
+  param_to_minimize);
 
+  std::cout << "================ END MINIMIZATION ================="
+  <<std::endl;
 
+  for(unsigned int i=0; i<result.size(); i++){
+  std::cout << result[i] << " ";
+  }
+  std::cout << std::endl;
 
+  std::cout<<"I am done."<<std::endl;
+  */
 
+  /*
+  std::vector<double> LLH_values;
+  std::vector<double> norm_values;
+  std::vector<double> gamma_values;
+  std::vector<double> r_kpi_values;
+  std::vector<double> r_nubarnu_values;
+  std::vector<double> param;
+  */
 
+  /*
+  //	loop over overall normalization values
+  LLH_values.clear();
+  norm_values.clear();
 
-/*
-std::shared_ptr<Verosimilitud> Vp = std::make_shared<Verosimilitud>(3,0,2);
 
-//std::vector<double> Verosimilitud::MinLLH(std::vector<double> param, std::vector<double> low_bound, std::vector<double> high_bound, std::vector<bool> param_to_minimize)
+  for( double	norm = 1.0; norm < 4.0; norm += 0.1)
+  {
+          param.clear();
 
-std::vector<double> param = {1.0, 0.01, 1.0, 1.0};
-std::vector<double> low_bound = {0.01, -0.05, 0.5, 0.875};
-std::vector<double> high_bound = {3.0, 0.05, 1.5, 1.125};
-std::vector<bool> param_to_minimize = {true, true, true, true};
+          param.push_back(norm);	// overall norm
+          param.push_back(0.0);	// change to spectral index
+          param.push_back(1.0);	// r_kpi
+          param.push_back(1.0);	// r_nubarnu
 
-std::cout << "================ BEGIN CODE =================" <<std::endl;
+          LLH_values.push_back(Vp->LLH(param));
+          norm_values.push_back(norm);
 
-dlib::matrix<double,0,1> r(4);
-for(size_t i=0; i<param.size(); i++) r(i)=param[i];
-std::cout << "Chi2Initial: "<< std::endl << Vp->Chi2(r) <<std::endl;
-std::cout << "Chi2GradientInitial: "<< std::endl << Vp->Chi2Gradient(r) <<std::endl;
 
-std::cout << "================ BEGIN MINIMIZATION =================" <<std::endl;
+  }
 
-std::vector<double> result = Vp->MinLLH(param, low_bound, high_bound, param_to_minimize);
+  for( int i = 0; i<LLH_values.size(); i++){
+          std::cout<< norm_values[i] << "," << LLH_values[i]/550 << ",";
+  }
+  */
 
-std::cout << "================ END MINIMIZATION =================" <<std::endl;
+  /*
+  //  loop over spectral index changes
 
-for(unsigned int i=0; i<result.size(); i++){
-std::cout << result[i] << " ";
-}
-std::cout << std::endl;
+  LLH_values.clear();
+  gamma_values.clear();
 
-std::cout<<"I am done."<<std::endl;
-*/
+  for( double	gamma = -1.0; gamma < 1.0; gamma += 0.05)
+  {
 
+          param.clear();
 
+          param.push_back(2.0);	// overall norm
+          param.push_back(gamma);	// change to spectral index
+          param.push_back(1.0);	// r_kpi
+          param.push_back(1.0);	// r_nubarnu
 
+          LLH_values.push_back(Vp->LLH(param));
+          gamma_values.push_back(gamma);
 
+  }
 
+  for( int i = 0; i<LLH_values.size(); i++){
+          std::cout<< gamma_values[i] << "," << LLH_values[i]/550 << ",";
+  }
+  */
 
+  /*
+  //	loop over k/pi ratio
+  LLH_values.clear();
+  r_kpi_values.clear();
 
 
+  for( double	r_kpi = 0.5; r_kpi < 1.5; r_kpi += 0.05)
+  {
+          param.clear();
 
+          param.push_back(2.0);	// overall norm
+          param.push_back(0.0);	// change to spectral index
+          param.push_back(r_kpi);	// r_kpi
+          param.push_back(1.0);	// r_nubarnu
 
+          LLH_values.push_back(Vp->LLH(param));
+          r_kpi_values.push_back(r_kpi);
 
 
+  }
 
+  for( int i = 0; i<LLH_values.size(); i++){
+          std::cout<< r_kpi_values[i] << "," << LLH_values[i]/550 << ",";
+  }
+  */
 
+  /*
+  loop over nubar/nu ratio
+  LLH_values.clear();
+  r_nubarnu_values.clear();
 
 
+  for( double	r_nubarnu = 0.875; r_nubarnu < 1.25; r_nubarnu += 0.01)
+  {
+          param.clear();
 
+          param.push_back(2.0);	// overall norm
+          param.push_back(0.0);	// change to spectral index
+          param.push_back(1.0);	// r_kpi
+          param.push_back(r_nubarnu);	// r_nubarnu
 
+          LLH_values.push_back(Vp->LLH(param));
+          r_nubarnu_values.push_back(r_nubarnu);
 
-/*
-std::vector<double> LLH_values;
-std::vector<double> norm_values;
-std::vector<double> gamma_values;
-std::vector<double> r_kpi_values;
-std::vector<double> r_nubarnu_values;
-std::vector<double> param;
-*/
 
+  }
 
-/*
-//	loop over overall normalization values
-LLH_values.clear();
-norm_values.clear();
+  for( int i = 0; i<LLH_values.size(); i++){
+          std::cout<< r_nubarnu_values[i] << "," << LLH_values[i]/550 << ",";
+  }
+  */
 
+  /*
+          std::vector<double> param;
+          param.push_back(1.0);	// overall norm
+          param.push_back(0.0);	// change to spectral index
+          param.push_back(1.0);	// r_kpi
+          param.push_back(1.0);	// r_nubarnu
 
-for( double	norm = 1.0; norm < 4.0; norm += 0.1)
-{
-	param.clear();
 
-	param.push_back(norm);	// overall norm
-	param.push_back(0.0);	// change to spectral index
-	param.push_back(1.0);	// r_kpi
-	param.push_back(1.0);	// r_nubarnu
-	
-	LLH_values.push_back(Vp->LLH(param));
-	norm_values.push_back(norm);
-	
+   std::cout << "LLH: "<< Vp->LLH(param) <<std::endl;
+  std::cout << "LLH: above!!!" <<std::endl;
 
-}
+  std::vector<double> mc = Vp->GetPertExpectationVec(param);
+  std::vector<double> data = Vp->GetDataVec();
 
-for( int i = 0; i<LLH_values.size(); i++){
-	std::cout<< norm_values[i] << "," << LLH_values[i]/550 << ",";
-}
-*/
+  const int datasize = data.size();
+  const int mcsize = mc.size();
 
-/*
-//  loop over spectral index changes
+  std::cout<< "MC: "<<std::endl;
+  for (int i = 0; i<mcsize; i++)
+  {
+          std::cout<<mc[i]<<","<<std::endl;
+  }
 
-LLH_values.clear();
-gamma_values.clear();
+  std::cout<< "data: "<<std::endl;
+  for (int i = 0; i<datasize; i++)
+  {
+          std::cout<<data[i]<<","<<std::endl;
+  }
+  */
 
-for( double	gamma = -1.0; gamma < 1.0; gamma += 0.05)
-{
-
-	param.clear();
-	
-	param.push_back(2.0);	// overall norm
-	param.push_back(gamma);	// change to spectral index
-	param.push_back(1.0);	// r_kpi
-	param.push_back(1.0);	// r_nubarnu
-	
-	LLH_values.push_back(Vp->LLH(param));
-	gamma_values.push_back(gamma);
-		
-}
-
-for( int i = 0; i<LLH_values.size(); i++){
-	std::cout<< gamma_values[i] << "," << LLH_values[i]/550 << ",";
-}
-*/
-
-/*
-//	loop over k/pi ratio
-LLH_values.clear();
-r_kpi_values.clear();
-
-
-for( double	r_kpi = 0.5; r_kpi < 1.5; r_kpi += 0.05)
-{
-	param.clear();
-
-	param.push_back(2.0);	// overall norm
-	param.push_back(0.0);	// change to spectral index
-	param.push_back(r_kpi);	// r_kpi
-	param.push_back(1.0);	// r_nubarnu
-	
-	LLH_values.push_back(Vp->LLH(param));
-	r_kpi_values.push_back(r_kpi);
-	
-
-}
-
-for( int i = 0; i<LLH_values.size(); i++){
-	std::cout<< r_kpi_values[i] << "," << LLH_values[i]/550 << ",";
-}
-*/
-
-/*
-loop over nubar/nu ratio
-LLH_values.clear();
-r_nubarnu_values.clear();
-
-
-for( double	r_nubarnu = 0.875; r_nubarnu < 1.25; r_nubarnu += 0.01)
-{
-	param.clear();
-
-	param.push_back(2.0);	// overall norm
-	param.push_back(0.0);	// change to spectral index
-	param.push_back(1.0);	// r_kpi
-	param.push_back(r_nubarnu);	// r_nubarnu
-	
-	LLH_values.push_back(Vp->LLH(param));
-	r_nubarnu_values.push_back(r_nubarnu);
-	
-
-}
-
-for( int i = 0; i<LLH_values.size(); i++){
-	std::cout<< r_nubarnu_values[i] << "," << LLH_values[i]/550 << ",";
-}
-*/
-
-
-
-/*
-	std::vector<double> param;
-	param.push_back(1.0);	// overall norm
-	param.push_back(0.0);	// change to spectral index
-	param.push_back(1.0);	// r_kpi
-	param.push_back(1.0);	// r_nubarnu
-
-
- std::cout << "LLH: "<< Vp->LLH(param) <<std::endl;
-std::cout << "LLH: above!!!" <<std::endl;
-
-std::vector<double> mc = Vp->GetPertExpectationVec(param);
-std::vector<double> data = Vp->GetDataVec();
-
-const int datasize = data.size();
-const int mcsize = mc.size();
-
-std::cout<< "MC: "<<std::endl;
-for (int i = 0; i<mcsize; i++)
-{
-	std::cout<<mc[i]<<","<<std::endl;
-}
-
-std::cout<< "data: "<<std::endl;
-for (int i = 0; i<datasize; i++)
-{
-	std::cout<<data[i]<<","<<std::endl;
-}
-*/
-
-
-return 0;
-	
+  return 0;
 }
